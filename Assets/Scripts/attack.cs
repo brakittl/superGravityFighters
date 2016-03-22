cff1d4633b24a114c4410a161de02fcc6c170817
hallingt <hallingt@umich.edu>
2016-03-22 01:04:00 -0400

    Test

--- Assets/Scripts/attack.cs
+++ Assets/Scripts/attack.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class attack : StateMachineBehaviour {
-
+	//Hello!!
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 		animator.SetBool("attack", false);
