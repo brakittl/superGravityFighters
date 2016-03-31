d495c330cc20fa73b01c1b99180fbe2bc1aac81c
hallingt <hallingt@umich.edu>
2016-03-31 18:32:40 -0400

    Update Map Borders

--- Assets/Scripts/MapBorder.cs
+++ Assets/Scripts/MapBorder.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class MapBorder : MonoBehaviour {
 
 	void OnTriggerExit2D(Collider2D other) {
-		if (other.tag == "Player" || other.tag == "bullet"){
+		if (other.tag == "bullet"){
 			if ((other.transform.position.y > (GetComponent<BoxCollider2D>().transform.position.y + (GetComponent<BoxCollider2D>().size.y / 2)))){
 				other.transform.position = new Vector3(other.transform.position.x, ((GetComponent<BoxCollider2D>().offset.y - (GetComponent<BoxCollider2D>().size.y / 2) + 0.1F)), 0);
 			}
@@ -16,6 +16,23 @@ public class MapBorder : MonoBehaviour {
 			}
 			else if ((other.transform.position.x < ((GetComponent<BoxCollider2D>().offset.x - GetComponent<BoxCollider2D>().size.x / 2)))){
 				other.transform.position = new Vector3(((GetComponent<BoxCollider2D>().offset.x + (GetComponent<BoxCollider2D>().size.x / 2) - 0.1F)),other.transform.position.y, 0);
+			}
+		}
+		if (other.tag == "player_child"){
+			Debug.Log(other.transform.parent.transform.position.y);
+			Debug.Log(other.transform.position.y);
+			Debug.Log(GetComponent<BoxCollider2D>().transform.position.y + (GetComponent<BoxCollider2D>().size.y / 2));
+			if ((other.transform.parent.transform.position.y > (GetComponent<BoxCollider2D>().transform.position.y + (GetComponent<BoxCollider2D>().size.y / 2)))){
+				other.transform.parent.transform.position = new Vector3(other.transform.transform.parent.position.x, ((GetComponent<BoxCollider2D>().offset.y - (GetComponent<BoxCollider2D>().size.y / 2) + 0.1F)), 0);
+			}
+			else if ((other.transform.parent.transform.position.y < ((GetComponent<BoxCollider2D>().offset.y - GetComponent<BoxCollider2D>().size.y / 2)))){
+				other.transform.parent.transform.position = new Vector3(other.transform.transform.parent.position.x, ((GetComponent<BoxCollider2D>().offset.y + (GetComponent<BoxCollider2D>().size.y / 2) - 0.1F)), 0);
+			}
+			else if ((other.transform.parent.transform.position.x > (GetComponent<BoxCollider2D>().transform.position.x + (GetComponent<BoxCollider2D>().size.x / 2)))){
+				other.transform.parent.transform.position = new Vector3(((GetComponent<BoxCollider2D>().offset.x - (GetComponent<BoxCollider2D>().size.x / 2) + 0.1F)), other.transform.parent.transform.position.y, 0);
+			}
+			else if ((other.transform.parent.transform.position.x < ((GetComponent<BoxCollider2D>().offset.x - GetComponent<BoxCollider2D>().size.x / 2)))){
+				other.transform.parent.transform.position = new Vector3(((GetComponent<BoxCollider2D>().offset.x + (GetComponent<BoxCollider2D>().size.x / 2) - 0.1F)),other.transform.parent.transform.position.y, 0);
 			}
 		}
 	}
