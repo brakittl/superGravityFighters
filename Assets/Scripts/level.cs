1ec7efb169d2d301e3fae4bb55f4525db5d43fc5
Mahraan <mahraan@gmail.com>
2016-04-02 02:50:27 -0400

    finished colliders in city level

--- Assets/Scripts/level.cs
+++ Assets/Scripts/level.cs
@@ -2,69 +2,73 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public enum GameMode {NONE, SURVIVAL, DEATHMATCH, REVERSE_TAG};
+public enum GameMode{ NONE, SURVIVAL, DEATHMATCH, REVERSE_TAG };
 
-public class Level : MonoBehaviour {
+public class Level : MonoBehaviour{
 
   public static Level S; 
-  public GameMode gamemode;// You can use this for selecting the GameMode right from the Map Screen
+  public GameMode gamemode; // You can use this for selecting the GameMode right from the Map Screen
   public int rt_point_limit = 100;
 
   public Vector3[] respawnPoints;
-    Texture2D black = new Texture2D(1, 1);
-  // Use this for initialization
+  Texture2D black = new Texture2D(1, 1);
+
   void Start(){
 
+    // ==[game mode]============================================================
+    // =========================================================================
 
     S = this;
 
-		if (gamemode == GameMode.NONE) { 
-			if (PlayerPrefs.GetString("GameMode") == "SURVIVAL") {
+		if(gamemode == GameMode.NONE){ 
+			if(PlayerPrefs.GetString("GameMode") == "SURVIVAL"){
 				gamemode = GameMode.SURVIVAL;
 			}
-			else if (PlayerPrefs.GetString("GameMode") == "DEATHMATCH") {
+			else if(PlayerPrefs.GetString("GameMode") == "DEATHMATCH"){
 				gamemode = GameMode.DEATHMATCH;
 			}
-			else if (PlayerPrefs.GetString("GameMode") == "REVERSE_TAG") {
+			else if(PlayerPrefs.GetString("GameMode") == "REVERSE_TAG"){
 				gamemode = GameMode.REVERSE_TAG;
 			} 
 			else Debug.LogError("INCORRECT GAMEMODE STRING PASSED IN FROM PLAYERPREFS. CHECK LEVEL SCRIPT");
 		}
-		else {
+		else{
 			Debug.Log("GAMEMODE ALREADY SET FROM LEVEL SCRIPT. CURRENT GAMEMODE: " + gamemode);
 			Debug.Log("TO LET GAMEMODE BE SET THRU LEVEL SELECT, MAKE SURE LEVEL SCRIPT'S GAMEMODE IS NONE");
-
 		}
 
-     //PlayerPrefs.SetString("P1", "orange_player");
-     //PlayerPrefs.SetString("P2", "none");
-     //PlayerPrefs.SetString("P3", "red_player");
-     //PlayerPrefs.SetString("P4", "none");
+    // ==[players]============================================================
+    // =========================================================================
 
-	PlayerPrefs.SetString("Player Name", "Foobar");
+    //PlayerPrefs.SetString("P1", "orange_player");
+    //PlayerPrefs.SetString("P2", "none");
+    //PlayerPrefs.SetString("P3", "red_player");
+    //PlayerPrefs.SetString("P4", "none");
+
+    PlayerPrefs.SetString("Player Name", "Foobar");
 
     Vector3 rot = new Vector3(0, 0, 0);
 
-    // p1
-    if(PlayerPrefs.GetString("P1") != "none"){
-      GameObject player1 = Instantiate(Resources.Load("__Prefabs/" + PlayerPrefs.GetString("P1")), returnPosition(0), Quaternion.Euler(rot)) as GameObject;
-      player1.SendMessage("SetPlayerNumber", 1);
-    }
-    // p2
-    if(PlayerPrefs.GetString("P2") != "none"){
-      GameObject player2 = Instantiate(Resources.Load("__Prefabs/" + PlayerPrefs.GetString("P2")), returnPosition(1), Quaternion.Euler(rot)) as GameObject;
-      player2.SendMessage("SetPlayerNumber", 2);
-    }
-    // p3
-    if(PlayerPrefs.GetString("P3") != "none"){
-      GameObject player3 = Instantiate(Resources.Load("__Prefabs/" + PlayerPrefs.GetString("P3")), returnPosition(2), Quaternion.Euler(rot)) as GameObject;
-      player3.SendMessage("SetPlayerNumber", 3);
-    }
-    // p4
-    if(PlayerPrefs.GetString("P4") != "none"){
-      GameObject player4 = Instantiate(Resources.Load("__Prefabs/" + PlayerPrefs.GetString("P4")), returnPosition(3), Quaternion.Euler(rot)) as GameObject;
-      player4.SendMessage("SetPlayerNumber", 4);
-    }
+    // // p1
+    // if(PlayerPrefs.GetString("P1") != "none"){
+    //   GameObject player1 = Instantiate(Resources.Load("__Prefabs/_players" + PlayerPrefs.GetString("P1")), returnPosition(0), Quaternion.Euler(rot)) as GameObject;
+    //   player1.SendMessage("SetPlayerNumber", 1);
+    // }
+    // // p2
+    // if(PlayerPrefs.GetString("P2") != "none"){
+    //   GameObject player2 = Instantiate(Resources.Load("__Prefabs/_players" + PlayerPrefs.GetString("P2")), returnPosition(1), Quaternion.Euler(rot)) as GameObject;
+    //   player2.SendMessage("SetPlayerNumber", 2);
+    // }
+    // // p3
+    // if(PlayerPrefs.GetString("P3") != "none"){
+    //   GameObject player3 = Instantiate(Resources.Load("__Prefabs/_players" + PlayerPrefs.GetString("P3")), returnPosition(2), Quaternion.Euler(rot)) as GameObject;
+    //   player3.SendMessage("SetPlayerNumber", 3);
+    // }
+    // // p4
+    // if(PlayerPrefs.GetString("P4") != "none"){
+    //   GameObject player4 = Instantiate(Resources.Load("__Prefabs/_players" + PlayerPrefs.GetString("P4")), returnPosition(3), Quaternion.Euler(rot)) as GameObject;
+    //   player4.SendMessage("SetPlayerNumber", 4);
+    // }
 
   }
 
@@ -72,49 +76,46 @@ public class Level : MonoBehaviour {
     return respawnPoints[i];
   }
 
-    public GameObject blackSquare;
-    GameObject topWall, bottomWall, leftWall, rightWall;
-    public bool pause = false, running = false;
-    int runAgain = 0;
-    public List<Vector3> positions = new List<Vector3>();
+  public GameObject blackSquare;
+  GameObject topWall, bottomWall, leftWall, rightWall;
+  public bool pause = false, running = false;
+  int runAgain = 0;
+  public List<Vector3> positions = new List<Vector3>();
 
-    public void KillPause(Vector3 playerPos)
-    {
-        if (!running)
-        {
-            running = true;
-            StartCoroutine(Pause(playerPos));
-        }     
-        
+  public void KillPause(Vector3 playerPos){
+    if(!running){
+      running = true;
+      StartCoroutine(Pause(playerPos));
     }
+  }
 
-    IEnumerator Pause(Vector3 pos)
-    {
-        //yield return new WaitForSeconds(0.05f);
-        Time.timeScale = 0.1f;
-        Vector3 cubePos = pos, rot = transform.rotation.eulerAngles;
+  IEnumerator Pause(Vector3 pos){
+    
+    //yield return new WaitForSeconds(0.05f);
+    Time.timeScale = 0.1f;
+    Vector3 cubePos = pos, rot = transform.rotation.eulerAngles;
 
-        cubePos.y = pos.y - 4.3f;
-        bottomWall = Instantiate(blackSquare, cubePos, transform.rotation) as GameObject;
-        cubePos.y = pos.y + 4.3f;
-        topWall = Instantiate(blackSquare, cubePos, transform.rotation) as GameObject;
-        cubePos.x = pos.x - 4.3f;
-        rot.z = 90;
-        leftWall = Instantiate(blackSquare, cubePos, Quaternion.Euler(rot)) as GameObject;
-        cubePos.x = pos.x + 4.3f;
-        rightWall = Instantiate(blackSquare, cubePos, Quaternion.Euler(rot)) as GameObject;
+    cubePos.y = pos.y - 4.3f;
+    bottomWall = Instantiate(blackSquare, cubePos, transform.rotation) as GameObject;
+    cubePos.y = pos.y + 4.3f;
+    topWall = Instantiate(blackSquare, cubePos, transform.rotation) as GameObject;
+    cubePos.x = pos.x - 4.3f;
+    rot.z = 90;
+    leftWall = Instantiate(blackSquare, cubePos, Quaternion.Euler(rot)) as GameObject;
+    cubePos.x = pos.x + 4.3f;
+    rightWall = Instantiate(blackSquare, cubePos, Quaternion.Euler(rot)) as GameObject;
 
-        yield return new WaitForSeconds(0.029f);
-        Destroy(topWall);
-        Destroy(bottomWall);
-        Destroy(leftWall);
-        Destroy(rightWall);
-        Time.timeScale = 1;
+    yield return new WaitForSeconds(0.029f);
+    Destroy(topWall);
+    Destroy(bottomWall);
+    Destroy(leftWall);
+    Destroy(rightWall);
+    Time.timeScale = 1;
 
-        running = false;
-    }
+    running = false;
+  }
 
-    public Vector3 findRespawn(){
+  public Vector3 findRespawn(){
     
     Vector3 respawnPoint = new Vector3(0, 0, 0);
     float closestPlayerDist = 10000f;
